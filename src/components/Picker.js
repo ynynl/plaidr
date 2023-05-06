aaf72131107ec4e5f3176305872b352bb00c86b7
ynynl <yinyin.lu@hellofresh.ca>
2023-05-05 23:51:07 -0400

    refactor color picker

--- src/components/Picker.js
+++ src/components/Picker.js
@@ -1,105 +1,95 @@
+import React from "react";
 
-import React from 'react';
-import { getRandomItems, randomPivots } from '../utils';
+export const ColorPicker = ({
+  numOfColor,
+  handleNewColor,
+  handleNewPivots,
+  handleNewNumOfColor,
+}) => {
+  const handleNumOfColorChange = (event) => {
+    const newNumOfColor = event.target.value;
+    handleNewNumOfColor(newNumOfColor);
+  };
 
-export const ColorPicker = ({ rgbArray, numOfColor, setColors, setPivots, setNumOfColor }) => {
-    const handleNumOfColorChange = (event) => {
-        const newNumOfColor = event.target.value;
-        setNumOfColor(newNumOfColor);
-        const newColors = getRandomItems(rgbArray, newNumOfColor);
-        const newPivots = randomPivots(newNumOfColor - 1);
-        setColors(newColors);
-        setPivots(newPivots);
-    };
-
-    const handleNewColor = () => {
-        setColors(getRandomItems(rgbArray, numOfColor));
-    };
-
-    const handleNewPivots = () => {
-        setPivots(randomPivots(numOfColor - 1));
-    };
-
-    return (
-        <div>
-            <label htmlFor="numOfColor">Number of Colors:</label>
-            <input
-                type="range"
-                id="numOfColor"
-                name="numOfColor"
-                min="3"
-                max="20"
-                value={numOfColor}
-                onChange={handleNumOfColorChange}
-            />
-            <div>
-                <button onClick={handleNewPivots}>Generate New Pivots</button>
-            </div>
-            <div>
-                <button onClick={handleNewColor}>Generate New Colors</button>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <label htmlFor="numOfColor">Number of Colors:</label>
+      <input
+        type="range"
+        id="numOfColor"
+        name="numOfColor"
+        min="3"
+        max="20"
+        value={numOfColor}
+        onChange={handleNumOfColorChange}
+      />
+      <div>
+        <button onClick={handleNewPivots}>Generate New Pivots</button>
+      </div>
+      <div>
+        <button onClick={handleNewColor}>Generate New Colors</button>
+      </div>
+    </div>
+  );
 };
 
 export const TwillPicker = ({ twill, setTwill }) => {
-    const handleTwillChange = (event) => {
-        setTwill(event.target.value);
-    };
+  const handleTwillChange = (event) => {
+    setTwill(event.target.value);
+  };
 
-    return (
-        <div>
-            <label htmlFor="twill">Twill:</label>
-            <input
-                type="radio"
-                id="tartan"
-                name="twill"
-                value="tartan"
-                checked={twill === "tartan"}
-                onChange={handleTwillChange}
-            />
-            <label htmlFor="tartan">Tartan</label>
-            <input
-                type="radio"
-                id="net"
-                name="twill"
-                value="net"
-                checked={twill === "net"}
-                onChange={handleTwillChange}
-            />
-            <label htmlFor="net">Net</label>
-            <input
-                type="radio"
-                id="madras"
-                name="twill"
-                value="madras"
-                checked={twill === "madras"}
-                onChange={handleTwillChange}
-            />
-            <label htmlFor="madras">Madras</label>
-        </div>
-    );
+  return (
+    <div>
+      <label htmlFor="twill">Twill:</label>
+      <input
+        type="radio"
+        id="tartan"
+        name="twill"
+        value="tartan"
+        checked={twill === "tartan"}
+        onChange={handleTwillChange}
+      />
+      <label htmlFor="tartan">Tartan</label>
+      <input
+        type="radio"
+        id="net"
+        name="twill"
+        value="net"
+        checked={twill === "net"}
+        onChange={handleTwillChange}
+      />
+      <label htmlFor="net">Net</label>
+      <input
+        type="radio"
+        id="madras"
+        name="twill"
+        value="madras"
+        checked={twill === "madras"}
+        onChange={handleTwillChange}
+      />
+      <label htmlFor="madras">Madras</label>
+    </div>
+  );
 };
 
 export const SizePicker = ({ size, setSize }) => {
-    const handleSizeChange = (event) => {
-        setSize(Number(event.target.value));
-    };
+  const handleSizeChange = (event) => {
+    setSize(Number(event.target.value));
+  };
 
-    return (
-        <div>
-            <label htmlFor="size">Size:</label>
-            <input
-                type="range"
-                id="size"
-                name="size"
-                min={64}
-                max={512}
-                step={64}
-                value={size}
-                onChange={handleSizeChange}
-            />
-            <p>Current size: {size}</p>
-        </div>
-    );
+  return (
+    <div>
+      <label htmlFor="size">Size: {size} </label>
+      <input
+        type="range"
+        id="size"
+        name="size"
+        min={64}
+        max={512}
+        step={64}
+        value={size}
+        onChange={handleSizeChange}
+      />
+    </div>
+  );
 };
--- src/components/PlaidGenerator.js
+++ src/components/PlaidGenerator.js
@@ -1,8 +1,7 @@
-
 import React, { useState, useEffect } from "react";
-import generatePlaid from "../generator";
-import { arrayToDataURL, getRandomItems, randomPivots } from "../utils";
-import { ColorPicker, TwillPicker, SizePicker } from './Picker'
+import generatePlaid from "../utils/generator";
+import { arrayToDataURL, getRandomItems, randomPivots } from "../utils/utils";
+import { ColorPicker, TwillPicker, SizePicker } from "./Picker";
 import ImageUploader from "./ImageUploader";
 
 const PlaidGenerator = () => {
@@ -12,19 +11,28 @@ const PlaidGenerator = () => {
     colors: [],
     size: 128,
     twill: "tartan",
-    pivots: []
+    pivots: [],
   });
   const [numOfColor, setNumOfColor] = useState(5);
   const [rgbArray, setRgbArray] = useState([]);
 
-  const initiatePlaid = (newRgbArray) => {
+  const handleNewRgbArray = (newRgbArray) => {
     setRgbArray(newRgbArray);
     setPlaidSettings({
       ...plaidSettings,
       colors: getRandomItems(newRgbArray, numOfColor),
-      pivots: randomPivots(numOfColor-1)
+      pivots: randomPivots(numOfColor - 1),
     });
-  }
+  };
+
+  const handleNewNumOfColor = (newNumOfColor) => {
+    setNumOfColor(newNumOfColor);
+    setPlaidSettings({
+      ...plaidSettings,
+      pivots: randomPivots(newNumOfColor - 1),
+      colors: getRandomItems(rgbArray, newNumOfColor),
+    });
+  };
 
   useEffect(() => {
     if (plaidSettings.colors.length > 0) {
@@ -36,26 +44,38 @@ const PlaidGenerator = () => {
 
   return (
     <div>
-      <ImageUploader
-        setImage={setImage}
-        initiatePlaid={initiatePlaid}
-      />
-      <ColorPicker
-        rgbArray={rgbArray}
-        numOfColor={numOfColor}
-        setNumOfColor={setNumOfColor}
-        setColors={(colors) => setPlaidSettings({...plaidSettings, colors})}
-        setPivots={(pivots) => setPlaidSettings({...plaidSettings, pivots})}
-      />
+      <ImageUploader setImage={setImage} handleNewRgbArray={handleNewRgbArray} />
       <TwillPicker
         twill={plaidSettings.twill}
-        setTwill={(twill) => setPlaidSettings({...plaidSettings, twill})}
+        setTwill={(twill) => setPlaidSettings({ ...plaidSettings, twill })}
       />
       <SizePicker
         size={plaidSettings.size}
-        setSize={(size) => setPlaidSettings({...plaidSettings, size})}
+        setSize={(size) => setPlaidSettings({ ...plaidSettings, size })}
       />
-      {image && <img src={image} alt="uploaded" style={{ width: "300px", height: "300px", objectFit: "cover" }} />}
+      <ColorPicker
+        numOfColor={numOfColor}
+        handleNewNumOfColor={handleNewNumOfColor}
+        handleNewColor={() =>
+          setPlaidSettings({
+            ...plaidSettings,
+            colors: getRandomItems(rgbArray, numOfColor),
+          })
+        }
+        handleNewPivots={() =>
+          setPlaidSettings({
+            ...plaidSettings,
+            pivots: randomPivots(numOfColor - 1),
+          })
+        }
+      />
+      {image && (
+        <img
+          src={image}
+          alt="uploaded"
+          style={{ width: "300px", height: "300px", objectFit: "cover" }}
+        />
+      )}
       {plaidImage && (
         <img
           src={plaidImage}
@@ -68,4 +88,3 @@ const PlaidGenerator = () => {
 };
 
 export default PlaidGenerator;
-
